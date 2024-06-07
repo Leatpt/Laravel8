22d51e8e657515cd7044598e125ab15c65f315bc
Leatpt <lea.kibur@tptlive.ee>
2024-06-07 21:06:43 +0300

    11 - Use a Filesystem Class to Read a Directory

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -9,66 +9,12 @@
 </head>
 
 <body>
+    <?php foreach ($posts as $post): ?>
     <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-            recusandae odio nisi dicta laboriosam quisquam iste qui quae! Exercitationem
-            et, dolor fugiat reiciendis laborum, praesentium inventore placeat porro
-            facere alias in nisi eligendi veritatis omnis accusamus numquam voluptatum
-            ad provident incidunt adipisci repudiandae? Obcaecati possimus pariatur id,
-            amet reprehenderit aperiam rem atque iusto, quae esse ipsum dolor minima
-            nobis suscipit. Sapiente, doloribus eos numquam repellat eius maiores
-            laborum at eaque esse vel culpa praesentium voluptas dolore cum dolorem,
-            animi architecto illo alias tenetur eveniet sequi! Sunt, nam! Maiores eos
-            non beatae, inventore assumenda iure totam cumque at molestias! Dolor
-            maiores libero rerum ab unde voluptas veritatis iusto perferendis! Hic sed
-            ex dignissimos. Enim ipsam, quia laborum soluta maiores itaque delectus,
-            inventore saepe explicabo a, sunt veniam? Delectus ducimus provident vitae.
-        </p>
-
+        <?= $post; ?>
     </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+    <?php endforeach; ?>
 
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-            recusandae odio nisi dicta laboriosam quisquam iste qui quae! Exercitationem
-            et, dolor fugiat reiciendis laborum, praesentium inventore placeat porro
-            facere alias in nisi eligendi veritatis omnis accusamus numquam voluptatum
-            ad provident incidunt adipisci repudiandae? Obcaecati possimus pariatur id,
-            amet reprehenderit aperiam rem atque iusto, quae esse ipsum dolor minima
-            nobis suscipit. Sapiente, doloribus eos numquam repellat eius maiores
-            laborum at eaque esse vel culpa praesentium voluptas dolore cum dolorem,
-            animi architecto illo alias tenetur eveniet sequi! Sunt, nam! Maiores eos
-            non beatae, inventore assumenda iure totam cumque at molestias! Dolor
-            maiores libero rerum ab unde voluptas veritatis iusto perferendis! Hic sed
-            ex dignissimos. Enim ipsam, quia laborum soluta maiores itaque delectus,
-            inventore saepe explicabo a, sunt veniam? Delectus ducimus provident vitae.
-        </p>
-
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-            recusandae odio nisi dicta laboriosam quisquam iste qui quae! Exercitationem
-            et, dolor fugiat reiciendis laborum, praesentium inventore placeat porro
-            facere alias in nisi eligendi veritatis omnis accusamus numquam voluptatum
-            ad provident incidunt adipisci repudiandae? Obcaecati possimus pariatur id,
-            amet reprehenderit aperiam rem atque iusto, quae esse ipsum dolor minima
-            nobis suscipit. Sapiente, doloribus eos numquam repellat eius maiores
-            laborum at eaque esse vel culpa praesentium voluptas dolore cum dolorem,
-            animi architecto illo alias tenetur eveniet sequi! Sunt, nam! Maiores eos
-            non beatae, inventore assumenda iure totam cumque at molestias! Dolor
-            maiores libero rerum ab unde voluptas veritatis iusto perferendis! Hic sed
-            ex dignissimos. Enim ipsam, quia laborum soluta maiores itaque delectus,
-            inventore saepe explicabo a, sunt veniam? Delectus ducimus provident vitae.
-        </p>
-
-    </article>
 </body>
 
 </html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,24 +1,14 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) { //wrapping in {} makes it into a wildcard
-
-    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        return redirect("/");
-    }
-
-    $post = cache()->remember(
-        "posts.{$slug}",
-        now()->addMinutes(20),
-        fn() => file_get_contents($path)
-    );
-
-    return view('post', [
-        'post' => $post
-    ]);
+    return view('post', ['post' => Post::find($slug)]);
 })->where('post', '[A-z_\-]+');
