21c516b4a51cb07027b2356e07aad1918987b8e9
Leatpt <lea.kibur@tptlive.ee>
2024-06-07 19:17:23 +0300

    7 - Make a Route and Link to it

---
+++ blog/resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque amet aut, officia reiciendis nam repellendus, et voluptatum repellat nesciunt fugiat a optio voluptates quidem ad odit animi provident quae voluptate.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est accusantium cupiditate repudiandae dolores neque harum delectus officiis esse eius repellendus optio quam blanditiis aliquam iste, doloribus sed magnam nam. Quia!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque amet aut, officia reiciendis nam repellendus, et voluptatum repellat nesciunt fugiat a optio voluptates quidem ad odit animi provident quae voluptate.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque amet aut, officia reiciendis nam repellendus, et voluptatum repellat nesciunt fugiat a optio voluptates quidem ad odit animi provident quae voluptate.
+        </p>
+    </article>
+</body>
+</html>
