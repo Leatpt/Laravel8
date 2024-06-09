dc12b6212e3167d042688e4ed384e8e493c1d383
Leatpt <lea.kibur@tptlive.ee>
2024-06-09 14:16:25 +0300

    25 - Show All Posts Associated With a Category

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
     @foreach ($posts as $post)
-        <article class="{{$loop->even ? 'foobar' : ''}}">
+        <article>
             <h1>
                 <a href="/posts/{{$post->slug}}">
                     {!!$post->title!!}
                 </a>
             </h1>
             <p>
-                <a href="/categories/{{$post->category->id}}">{{ $post->category->name }}</a>
+                <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
             </p>
             <div>
                 {{$post->excerpt}}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,7 +18,7 @@ Route::get('posts/{post:slug}', function (Post $post) { //wrapping in {} makes i
     ]);
 });
 
-Route::get('categories/{categories}', function (Category $category) {
+Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
         'posts' => $category->posts
