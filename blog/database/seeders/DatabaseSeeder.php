165caf964bac7a25077a33f005ab15088eb58c28
Leatpt <lea.kibur@tptlive.ee>
2024-06-09 16:27:17 +0300

    28 - Turbo Boost With Factories

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -15,45 +15,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+        // Don't need to trunctate, with the assumption
+        // that you only run the seed after migrate:fresh
+        // User::truncate();
+        // Category::truncate();
+        // Post::truncate();
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
+        $user = User::factory()->create([
+            'name' => 'Jae Kaap'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        Post::create([
+        Post::factory(5)->create([
             'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-first-post',
-            'excerpt' => '<p>Excerpt for my post</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia culpa velit, id assumenda obcaecati repudiandae repellendus distinctio maiores sint perferendis nisi fugit corporis maxime eaque, excepturi dolore odit dolorum praesentium. Aut tempore laborum corporis quae ullam ex, sunt qui, suscipit rerum asperiores neque magnam fugit totam. Dolorem tenetur amet harum, dolore maiores facere dolores, sapiente eaque vero quo voluptates, veritatis sint debitis minus commodi magni asperiores possimus nemo dolor soluta perspiciatis! Iure doloremque, aperiam, itaque architecto doloribus quibusdam voluptatum in sint dicta alias eum ab dolorem aut culpa tempore! Obcaecati officiis, error vero hic consectetur mollitia. Inventore qui iste sequi illum dolorum enim voluptatem dolorem doloribus ab voluptas. Iste ipsum rerum dolor a. Ratione quis odio minus ex nam itaque, debitis distinctio sunt cum quos hic mollitia similique libero quo dicta sapiente voluptatem quaerat tempora. Omnis quasi commodi est fuga reiciendis, quos, ad nisi, corporis rerum aperiam aliquid. Nesciunt, pariatur.
-            </p>',
         ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-second-post',
-            'excerpt' => '<p>Excerpt for my post</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia culpa velit, id assumenda obcaecati repudiandae repellendus distinctio maiores sint perferendis nisi fugit corporis maxime eaque, excepturi dolore odit dolorum praesentium. Aut tempore laborum corporis quae ullam ex, sunt qui, suscipit rerum asperiores neque magnam fugit totam. Dolorem tenetur amet harum, dolore maiores facere dolores, sapiente eaque vero quo voluptates, veritatis sint debitis minus commodi magni asperiores possimus nemo dolor soluta perspiciatis! Iure doloremque, aperiam, itaque architecto doloribus quibusdam voluptatum in sint dicta alias eum ab dolorem aut culpa tempore! Obcaecati officiis, error vero hic consectetur mollitia. Inventore qui iste sequi illum dolorum enim voluptatem dolorem doloribus ab voluptas. Iste ipsum rerum dolor a. Ratione quis odio minus ex nam itaque, debitis distinctio sunt cum quos hic mollitia similique libero quo dicta sapiente voluptatem quaerat tempora. Omnis quasi commodi est fuga reiciendis, quos, ad nisi, corporis rerum aperiam aliquid. Nesciunt, pariatur.
-            </p>',
-        ]);
+        // $user = User::factory()->create();
+
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal',
+        // ]);
+
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family',
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work',
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My Family Post',
+        //     'slug' => 'my-first-post',
+        //     'excerpt' => '<p>Excerpt for my post</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia culpa velit, id assumenda obcaecati repudiandae repellendus distinctio maiores sint perferendis nisi fugit corporis maxime eaque, excepturi dolore odit dolorum praesentium. Aut tempore laborum corporis quae ullam ex, sunt qui, suscipit rerum asperiores neque magnam fugit totam. Dolorem tenetur amet harum, dolore maiores facere dolores, sapiente eaque vero quo voluptates, veritatis sint debitis minus commodi magni asperiores possimus nemo dolor soluta perspiciatis! Iure doloremque, aperiam, itaque architecto doloribus quibusdam voluptatum in sint dicta alias eum ab dolorem aut culpa tempore! Obcaecati officiis, error vero hic consectetur mollitia. Inventore qui iste sequi illum dolorum enim voluptatem dolorem doloribus ab voluptas. Iste ipsum rerum dolor a. Ratione quis odio minus ex nam itaque, debitis distinctio sunt cum quos hic mollitia similique libero quo dicta sapiente voluptatem quaerat tempora. Omnis quasi commodi est fuga reiciendis, quos, ad nisi, corporis rerum aperiam aliquid. Nesciunt, pariatur.
+        //     </p>',
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My Work Post',
+        //     'slug' => 'my-second-post',
+        //     'excerpt' => '<p>Excerpt for my post</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia culpa velit, id assumenda obcaecati repudiandae repellendus distinctio maiores sint perferendis nisi fugit corporis maxime eaque, excepturi dolore odit dolorum praesentium. Aut tempore laborum corporis quae ullam ex, sunt qui, suscipit rerum asperiores neque magnam fugit totam. Dolorem tenetur amet harum, dolore maiores facere dolores, sapiente eaque vero quo voluptates, veritatis sint debitis minus commodi magni asperiores possimus nemo dolor soluta perspiciatis! Iure doloremque, aperiam, itaque architecto doloribus quibusdam voluptatum in sint dicta alias eum ab dolorem aut culpa tempore! Obcaecati officiis, error vero hic consectetur mollitia. Inventore qui iste sequi illum dolorum enim voluptatem dolorem doloribus ab voluptas. Iste ipsum rerum dolor a. Ratione quis odio minus ex nam itaque, debitis distinctio sunt cum quos hic mollitia similique libero quo dicta sapiente voluptatem quaerat tempora. Omnis quasi commodi est fuga reiciendis, quos, ad nisi, corporis rerum aperiam aliquid. Nesciunt, pariatur.
+        //     </p>',
+        // ]);
     }
 }
