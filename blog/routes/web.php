5e0233cc8134ad4a87d9ef0fb60e203b97dbb3e7
Leatpt <lea.kibur@tptlive.ee>
2024-06-09 22:21:26 +0300

    34.5

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -23,13 +23,15 @@ Route::get('posts/{post:slug}', function (Post $post) { //wrapping in {} makes i
 Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
 
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
