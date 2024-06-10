a37d8b169fc517acd0acd81f7fce04a0263cc5d4
Leatpt <lea.kibur@tptlive.ee>
2024-06-10 20:51:56 +0300

    35 - How to Extract a Dropdown Blade Component

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', function () {
         'posts' => $posts,
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) { //wrapping in {} makes it into a wildcard
     return view('post', [
@@ -29,7 +29,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
 
