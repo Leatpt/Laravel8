5ff5fd9116751504fc15665cd3a3f385790cf31d
Leatpt <lea.kibur@tptlive.ee>
2024-06-12 22:26:41 +0300

    53 - Table Consistency and Foreign Key Constraints

--- blog/database/migrations/2024_06_12_191107_create_comments_table.php
+++ blog/database/migrations/2024_06_12_191107_create_comments_table.php
@@ -4,22 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
-     * @return void
      */
-
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id');
-            $table->string('slug')->unique();
-            $table->string('title');
-            $table->text('excerpt');
             $table->text('body');
             $table->timestamps();
         });
@@ -30,7 +24,6 @@ class CreatePostsTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
-}
-;
+};
