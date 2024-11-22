c4c07b33d9173a56456dae25c775de7a13f46ecc
Leatpt <lea.kibur@tptlive.ee>
2024-11-22 19:49:47 +0200

    56 - Activate the Comment Form

--- blog/resources/views/components/post-comment.blade.php
+++ blog/resources/views/components/post-comment.blade.php
@@ -3,12 +3,13 @@
 <x-panel class="bg-gray-100">
     <article class="flex space-x-4">
         <div style="flex-shrink:0">
-            <img src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="" style="width:100px" class="rounded-xl">
+            <img src="https://i.pravatar.cc/100?u={{ $comment->user_id }}" alt="" style="width:100px"
+                class="rounded-xl">
         </div>
         <div>
             <header class="mb-4">
                 <h3 class="font-bold">{{ $comment->author->username }}</h3>
-                <p class="text-xs">Posted <time>{{ $comment->created_at }}</time>.</p>
+                <p class="text-xs">Posted <time>{{ $comment->created_at->format('F j, Y, g:i a') }}</time>.</p>
             </header>
             <p>
                 {{ $comment->body }}
