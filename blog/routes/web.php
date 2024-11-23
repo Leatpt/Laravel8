9286fdcae76dd6a9f31c0944ffd83f923a71143e
Leatpt <lea.kibur@tptlive.ee>
2024-11-23 18:21:47 +0200

    60 - Extract a Newsletter Service

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -4,36 +4,16 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
+use App\Http\Controllers\NewsletterController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('newsletter', function () {
-    request()->validate(['email' => 'required | email']);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us10'
-    ]);
-
-    try {
-        $response = $mailchimp->lists->addListMember('a95713ca93', [
-            'email_address' => request('email'),
-            'status' => 'subscribed',
-        ]);
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
-    }
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
